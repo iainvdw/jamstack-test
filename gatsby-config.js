f0925e49dc8110c7d3f1867cabcfcab72efa6870
Iain van der Wiel <iain.vanderwiel@gmail.com>
2018-05-22 16:27:32 +0200

    Added offline and manifest plugins for PWA feature

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
 
-dotenv.config()
+dotenv.config();
 
 module.exports = {
   siteMetadata: {
@@ -9,11 +9,24 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-source-contentful`,
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Gatsby POC',
+        short_name: 'GatsbyPOC',
+        start_url: '/',
+        background_color: '#b4da55',
+        theme_color: '#b4444d',
+        display: 'minimal-ui',
+        icon: 'static/images/icons/icon.png', // This path is relative to the root of the site.
+      },
+    },
+    'gatsby-plugin-offline',
+    {
+      resolve: 'gatsby-source-contentful',
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
   ],
-}
+};
