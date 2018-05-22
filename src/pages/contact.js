acfdf99f091028118674a1ed0252ccb8a70a4b51
Iain van der Wiel <iain.vanderwiel@gmail.com>
2018-05-22 16:39:04 +0200

    Added Netlify contact form

---
+++ src/pages/contact.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Link from 'gatsby-link';
+
+const ContactPage = () => (
+  <div>
+    <h1>This is the homepage</h1>
+    <form netlify enctype="multipart/form-data">
+      <label htmlFor="name">Name</label>
+      <input type="text" id="name" />
+      <br />
+      <label htmlFor="email">E-mail</label>
+      <input type="email" id="email" />
+      <br />
+      <label htmlFor="file">File</label>
+      <input type="file" id="file" />
+      <br />
+      <button type="submit">Send</button>
+    </form>
+    <Link to="/">Go to homepage</Link>
+  </div>
+);
+
+export default ContactPage;
--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Link from 'gatsby-link';
 
-const HomePage = ({ data }) => (
+const HomePage = () => (
   <div>
     <h1>This is the homepage</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <Link to="/articles/">Go to articles</Link>
+    <Link to="/contact/">Contact us</Link>
   </div>
 );
 
