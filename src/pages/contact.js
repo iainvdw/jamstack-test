5f7465f21a20a2fdcc13613ca664764d2241d1d5
Iain van der Wiel <iain.vanderwiel@gmail.com>
2018-05-23 09:01:45 +0200

    Will this fix the form...? 🤔

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -11,6 +11,8 @@ const ContactPage = () => (
       data-netlify="true"
       data-netlify-honeypot="bot-field"
     >
+      <input type="hidden" name="form-name" value="contact" />  
+
       <p>
         <label>
           Your Name: <input type="text" name="name" />
